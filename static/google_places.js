035ac21af69fe00b67c53b32c182d363e88e47f4
Abhiram970 <abhiramp428@gmail.com>
2022-09-07 23:27:49 +0530

    Till now

--- static/google_places.js
+++ static/google_places.js
@@ -1,4 +1,4 @@
-
+var google_api_key = "AIzaSyDIcmJ-M505THCYxPhNM4UDTZHjt395cxI"
 $.getScript( "https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "&libraries=places") 
 .done(function( script, textStatus ) {
     google.maps.event.addDomListener(window, "load", initAutoComplete)
